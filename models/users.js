37ea7c9ac352171f41ca4052a035d9debd1314b6
Galina Khusnutdinova <khusnutdinova.gal@yandex.ru>
2022-07-07 08:49:22 +0300

    feat: исправление работы по правкам ревьюера

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { default: isEmail } = require('validator/lib/isEmail');
+const { regex } = require('../utils/utils');
 
 // Опишем схему:
 const userSchema = new mongoose.Schema({
@@ -35,6 +36,11 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validator: {
+      validate: {
+        match: [regex, 'Пожалуйста, заполните действительный URL-адрес'],
+      },
+    },
   },
 });
 
